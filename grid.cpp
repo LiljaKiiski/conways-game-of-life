4790a93b55a6d798c01ec9c55bedacfdd26b951f
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2024-01-01 21:48:47 -0800

    guess who has a working grid :D

--- grid.cpp
+++ grid.cpp
@@ -6,14 +6,24 @@ class Grid {
     private:
         int width;
         int height;
+        int size;
         bool* grid;
+
+        void init_grid(){
+            for (int i = 0; i < size; i++){
+                grid[i] = false;
+            }
+        }
 
     public:
         Grid(int width, int height){
             this->width = width;
             this-> height = height;
+            this->size = width*height;
 
-            *grid = new bool[width * height];
+            grid = new bool[size];
+
+            init_grid();
 
             cout << grid[0] << endl;
         }
